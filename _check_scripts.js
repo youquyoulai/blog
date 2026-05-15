807aadbb2ffd15f78fbc9372fc5a6af75670ad00
youquyoulai <youquyoulai@126.com>
2026-05-15 15:28:24 +0800

    chore: 删除调试脚本

--- _check_scripts.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const h = fs.readFileSync('public/search/index.html', 'utf8');
-const scripts = h.match(/<script[^>]*>/g).filter(s => s.includes('src=') && !s.includes('type='));
-console.log('Scripts with src:');
-scripts.forEach(s => console.log(' ', s));
--- _debug.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const h = fs.readFileSync('public/search/index.html', 'utf8');
-const scripts = h.match(/<script[^>]*>/g);
-const debug = h.match(/search-layout[^<]*/);
-const body = h.match(/body[^>]*>/);
-console.log('Body:', body ? body[0] : '?');
-console.log('Scripts:', scripts ? scripts.join('\n') : 'none');
-console.log('Debug:', debug ? debug[0] : '无');
